6a353ddef205aeca86a65674cd07a1a6a4d6acb9
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-11-04 01:34:15 +0000

    post method has created

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -5,14 +5,30 @@ const Users = ({ userPromise }) => {
     // const users = use(userPromise);
     console.log(userPromise);
     const users = use(userPromise);
+
     console.log(users);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        console.log(name, email);
+    }
     return (
         <div>
+            <div>
+                <form onSubmit={handleSubmit} action="">
+                    <input name="name" type="text" />
+                    <br />
+                    <input name="email" type="text" />
+                    <br />
+                    <button>Add User</button>
+                </form>
+            </div>
             {
-                users.map((user) => <p key={user.id}>{user.name}</p>)
+                users.map((user) => <p key={user.id}>{user.name} : {user.email}</p>)
             }
         </div>
     );
 };
-
 export default Users;
